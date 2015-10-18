844816dc3ba6f4fe7c40926c6bd17858f0945a37
Mike Stewart <thewonderidiot@gmail.com>
2015-10-18 15:56:27 -0700

    Timer module updates, now simulates ~half of A02-2

--- timer_test.v
+++ timer_test.v
@@ -8,15 +8,17 @@ module main;
 
     reg vcc = 1;
     reg gnd = 0;
-    wire phs2, phs2_n, phs4, phs4_n, rt, ct, ct_n, wt, wt_n, ovfstb_n;
     reg stop = 0;
+    wire phs2, phs2_n, phs3_n, phs4, phs4_n, rt, rt_n, wt, wt_n, ct, ct_n, tt_n, ovfstb_n, clk, p01, p01_n, p02, p02_n, p03, p03_n, p04, p04_n, p05, p05_n;
 
-    timer a1(vcc, gnd, rst, clock, phs2, phs2_n, phs4, phs4_n, rt, ct, ct_n, wt, wt_n, stop, ovfstb_n);
+    timer a1(vcc, gnd, rst, clock, stop, phs2, phs2_n, phs3_n, phs4, phs4_n, rt, rt_n, wt, wt_n, ct, ct_n, tt_n, ovfstb_n, clk, p01, p01_n, p02, p02_n, p03, p03_n, p04, p04_n, p05, p05_n);
     
     initial begin
         $dumpfile("dump.lxt");
         $dumpvars(0, main);
         #10 rst = 0;
-        #100000 $finish;
+        #1000000 stop = 1;
+        #2000000 stop = 0;
+        #5000000 $finish;
     end
 endmodule
