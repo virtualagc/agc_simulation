326626bef006fcf9d88b358b2a36890e551d3c51
Mike Stewart <thewonderidiot@gmail.com>
2015-10-23 21:51:24 -0700

    Updates for finishing of core timer module logic. It now needs to receive the GOJ1 signal to start full operation!

--- timer_test.v
+++ timer_test.v
@@ -17,20 +17,22 @@ module main;
     reg strt2 = 0;
     reg goj1 = 0;
 
-    timer a1(vcc, gnd, rst, clock, mstrtp, mstp, phs2, phs2_n, phs3_n, phs4, phs4_n, rt, rt_n, wt, wt_n, ct, ct_n, clk, tt_n, p01, p01_n, p02, p02_n, p03, p03_n, p04, p04_n, p05, p05_n, sby, alga, strt1, strt2, goj1, stopa, gojam, gojam_n, stop, stop_n, monwt, q2a, mgojam, mstpit_n);
+    reg wl15 = 0;
+    reg wl15_n = 1;
+    reg wl16 = 0;
+    reg wl16_n = 1;
+
+    timer a1(vcc, gnd, rst, clock, mstrtp, mstp, phs2, phs2_n, phs3_n, phs4, phs4_n, rt, rt_n, wt, wt_n, ct, ct_n, clk, tt_n, p01, p01_n, p02, p02_n, p03, p03_n, p04, p04_n, p05, p05_n, sby, alga, strt1, strt2, goj1, stopa, gojam, gojam_n, stop, stop_n, wl15, wl15_n, wl16, wl16_n, monwt, q2a, mgojam, mstpit_n);
     
     initial begin
         $dumpfile("dump.lxt");
         $dumpvars(0, main);
         #10 rst = 0;
-        #1000000 mstp = 1;
-        #20000 mstp = 0;
-        #200000 mstrtp = 1;
-        #20000 mstrtp = 0;
-        #50000 goj1 = 1;
-        #20000 goj1 = 0;
-        #50000 strt1 = 1;
-        #20000 strt1 = 0;
-        #5000000 $finish;
+        #1000 strt1 = 1;
+        #1000 strt1 = 0;
+        #5000 goj1 = 1;
+        #1000 goj1 = 0;
+        #5000000 strt1 = 1;
+        #5000 $finish;
     end
 endmodule
