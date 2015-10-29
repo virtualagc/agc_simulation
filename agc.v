16bc384837d000a9db9405dbb1a56eb1c3b472af
Mike Stewart <thewonderidiot@gmail.com>
2015-10-28 19:52:23 -0700

    Updates for the completed SQ register module. Getting really close to having enough logic to exit GOJAM -- all that's left is the stage 1 register

--- agc.v
+++ agc.v
@@ -19,7 +19,6 @@ module main;
     reg alga = 0;
     reg strt1 = 0;
     reg strt2 = 0;
-    reg goj1 = 0;
     reg wl16 = 0;
     reg wl16_n = 1;
     reg wl15 = 0;
@@ -53,9 +52,19 @@ module main;
     reg mnhrpt = 0;
     reg mtcsai = 0;
 
+    reg st0_n = 0;
+    reg st1_n = 1;
+    reg st3_n = 1;
+    reg std2 = 0;
+    reg br2_n = 1;
+    reg br1b2b = 0;
+    reg rxor0 = 0;
+
+    wire goj1;
+
     scaler a1(vcc, gnd, rst, fs01_n, rchat_n, rchbt_n);
     timer a2(vcc, gnd, rst, clock, mstrtp, mstp, phs2, phs2_n, phs3_n, phs4, phs4_n, rt, rt_n, wt, wt_n, ct, ct_n, clk, tt_n, p01, p01_n, p02, p02_n, p03, p03_n, p04, p04_n, p05, p05_n, sby, alga, strt1, strt2, goj1, stopa, gojam, gojam_n, stop, stop_n, wl15, wl15_n, wl16, wl16_n, fs01_n, t01, t01_n, t02, t02_n, t03, t03_n, t04, t04_n, t05, t05_n, t06, t06_n, t07, t07_n, t08, t08_n, t09, t09_n, t10, t10_n, t11, t11_n, t12, t12_n, monwt, q2a, mgojam, mstpit_n);
-    sq_register a3(vcc, gnd, rst, gojam, nisq, t02, t12_n, phs2_n, rt_n, ct_n, wt_n, wl16_n, wl14_n, wl13_n, wl12_n, wl11_n, wl10_n, ext, extpls, inkl, inkbt1, relpls, inhlpls, ovnhrp, ruptor_n, krpt, n5xp4, mnhrpt, mtcsai);
+    sq_register a3(vcc, gnd, rst, gojam, nisq, t01_n, t02, t12_n, phs2_n, rt_n, ct_n, wt_n, wl16_n, wl14_n, wl13_n, wl12_n, wl11_n, wl10_n, ext, extpls, inkl, relpls, inhlpls, ovnhrp, ruptor_n, krpt, n5xp4, st0_n, st1_n, st3_n, std2, br2_n, br1b2b, rxor0, goj1, mnhrpt, mtcsai);
 
     initial begin
         $dumpfile("dump.lxt");
@@ -63,8 +72,10 @@ module main;
         #5000 rst = 0;
         #10000 strt1 = 1;
         #1000  strt1 = 0;
-        #50000 goj1 = 1;
-        #1000  goj1 = 0;
+        #10000 st0_n = 1;
+        st1_n = 0;
+        #1000 st1_n = 1;
+        st0_n = 0;
         #20000 wl13 = 1;
         wl13_n = 0;
         #5000 nisq = 1;
