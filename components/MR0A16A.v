9c6fb70b4a0eaa81aaaa17f04904a55d007d4e3f
thewonderidiot <thewonderidiot@gmail.com>
2016-03-04 21:08:43 -0800

    FPGA support for MR0A16A fixed memory, plus SignalTap
    This uses internal memory, and is pretty straightforward after the
    addition of the new bidir codegen flag.

--- components/MR0A16A.v
+++ components/MR0A16A.v
@@ -1,7 +1,11 @@
 `timescale 1ns/1ps
 `default_nettype none
 
-module MR0A16A (A0, A1, A2, A3, A4, E_n, DQL0, DQL1, DQL2, DQL3, VDD1, VSS1, DQL4, DQL5, DQL6, DQL7, W_n, A5, A6, A7, A8, A9, A10, A11, A12, VSS2, VDD2, DC, DQU8, DQU9, DQU10, DQU11, VDD3, VSS3, DQU12, DQU13, DQU14, DQU15, LB_n, UB_n, G_n, A13, A14, A15, SIM_RST, SIM_CLK);
+module MR0A16A (A0, A1, A2, A3, A4, E_n, DQL0, DQL1, DQL2, DQL3, VDD1, VSS1, DQL4, DQL5, DQL6, DQL7, W_n, A5, A6, A7, A8, A9, A10, A11, A12, VSS2, VDD2, DC, DQU8, DQU9, DQU10, DQU11, VDD3, VSS3, DQU12, DQU13, DQU14, DQU15, LB_n, UB_n, G_n, A13, A14, A15, SIM_RST, SIM_CLK
+`ifdef TARGET_FPGA
+, DQL0_in, DQL1_in, DQL2_in, DQL3_in, DQL4_in, DQL5_in, DQL6_in, DQL7_in, DQU8_in, DQU9_in, DQU10_in, DQU11_in, DQU12_in, DQU13_in, DQU14_in, DQU15_in
+`endif
+);
     localparam delay = 30;
 
     input wire SIM_RST;
@@ -56,8 +60,27 @@ module MR0A16A (A0, A1, A2, A3, A4, E_n, DQL0, DQL1, DQL2, DQL3, VDD1, VSS1, DQL
 
     input wire DC;
 
-    wire [11:0] addr;
-    assign addr = {A11, A10, A9, A8, A7, A6, A5, A4, A3, A2, A1, A0};
+`ifdef TARGET_FPGA
+    input wire DQL0_in;
+    input wire DQL1_in;
+    input wire DQL2_in;
+    input wire DQL3_in;
+    input wire DQL4_in;
+    input wire DQL5_in;
+    input wire DQL6_in;
+    input wire DQL7_in;
+    input wire DQU8_in;
+    input wire DQU9_in;
+    input wire DQU10_in;
+    input wire DQU11_in;
+    input wire DQU12_in;
+    input wire DQU13_in;
+    input wire DQU14_in;
+    input wire DQU15_in;
+`endif
+
+    wire [10:0] addr;
+    assign addr = {A10, A9, A8, A7, A6, A5, A4, A3, A2, A1, A0};
 
     wire [15:0] sensed_word;
     wire [15:0] write_word;
@@ -80,9 +103,13 @@ module MR0A16A (A0, A1, A2, A3, A4, E_n, DQL0, DQL1, DQL2, DQL3, VDD1, VSS1, DQL
     assign DQU15 = sensed_word[15];
 
 `ifdef TARGET_FPGA
-    always @(posedge SIM_CLK) begin
-        // TODO
-    end
+	wire wren;
+	assign wren = !W_n;
+	wire [15:0] read_word;
+	assign sensed_word = (!E_n && !G_n) ? read_word : 16'b0;
+	assign write_word = {DQU15_in, DQU14_in, DQU13_in, DQU12_in, DQU11_in, DQU10_in, DQU9_in, DQU8_in, DQL7_in, DQL6_in, DQL5_in, DQL4_in, DQL3_in, DQL2_in, DQL1_in, DQL0_in};
+    
+	core_memory ram(addr, SIM_CLK, write_word, wren, read_word);
 `else
     reg [15:0] ram[0:2047];
     assign #delay write_word = {DQU15, DQU14, DQU13, DQU12, DQU11, DQU10, DQU9, DQU8, DQL7, DQL6, DQL5, DQL4, DQL3, DQL2, DQL1, DQL0};
